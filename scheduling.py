2c899df8605ae468d070a48fd5c069f980b8a913
shovarnu <shovid2019@gmail.com>
2023-09-19 00:36:47 +0530

    scheduling

---
+++ scheduling.py
@@ -0,0 +1,22 @@
+""" Two threads chopping vegetables """
+
+import threading
+import time
+
+chopping = True
+
+def vegetable_chopper():
+    name = threading.current_thread().getName()
+    vegetable_count = 0
+    while chopping:
+        print(name, 'chopped a vegetable!')
+        vegetable_count += 1
+    print(name, 'chopped', vegetable_count, 'vegetables.')
+
+if __name__ == '__main__':
+    threading.Thread(target=vegetable_chopper, name='Barron').start()
+    threading.Thread(target=vegetable_chopper, name='Olivia').start()
+ 
+    time.sleep(1) # chop veggies for 1 sec
+    chopping = False # Stop both threads from chopping
+    
